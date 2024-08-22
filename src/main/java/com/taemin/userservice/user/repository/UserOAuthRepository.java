81572f95adfdff19489cf13bd10ace25bffd716c
곽태민 <taminging@kakao.com>
2024-08-22 15:02:17 +0900

    fix: UserOAuthRepository.java enum query method x -> jpql 변경

--- src/main/java/com/taemin/userservice/user/repository/UserOAuthRepository.java
+++ src/main/java/com/taemin/userservice/user/repository/UserOAuthRepository.java
@@ -5,9 +5,12 @@ import com.taemin.userservice.user.domain.UserOAuth;
 import com.taemin.userservice.user.type.OAuthProvider;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserOAuthRepository extends JpaRepository<UserOAuth, Long> {
-    Optional<UserOAuth> findByUserAndOAuthProvider(User user, OAuthProvider oAuthProvider);
+    @Query("SELECT u FROM UserOAuth u WHERE u.user = :user AND u.oAuthProvider = :oAuthProvider")
+    Optional<UserOAuth> findByUserAndOAuthProvider(@Param("user") User user, @Param("oAuthProvider") OAuthProvider oAuthProvider);
 }
