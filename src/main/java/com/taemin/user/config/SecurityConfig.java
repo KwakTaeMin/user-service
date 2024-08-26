6a58b6e0702b0c10e938d466643c9f5b23b87367
곽태민 <taminging@kakao.com>
2024-08-26 09:24:41 +0900

    feat: swagger init 인증 없이 /swagger/index.html 접근

--- src/main/java/com/taemin/user/config/SecurityConfig.java
+++ src/main/java/com/taemin/user/config/SecurityConfig.java
@@ -25,38 +25,39 @@ public class SecurityConfig {
     private final TokenExceptionFilter tokenExceptionFilter;
     private final CustomOAuth2UserService oAuth2UserService;
     private final OAuthAuthenticationSuccessHandler oAuthAuthenticationSuccessHandler;
+
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring()
-                .requestMatchers("/error", "/favicon.ico");
+            .requestMatchers("/error", "/favicon.ico","/swagger-ui/**", "/v3/api-docs/**");
     }
 
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable) // cors 비활성화 -> 프론트와 연결 시 따로 설정 필요
-                .httpBasic(AbstractHttpConfigurer::disable) // 기본 인증 로그인 비활성화
-                .formLogin(AbstractHttpConfigurer::disable) // 기본 login form 비활성화
-                .logout(AbstractHttpConfigurer::disable) // 기본 logout 비활성화
-                .headers(c -> c.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable).disable()) // X-Frame-Options 비활성화
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // session 미사용
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/oauth2/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(tokenExceptionFilter, UsernamePasswordAuthenticationFilter.class)
-                .oauth2Login(oauth -> // oauth2 설정 -> OAuth2 로그인 기능에 대한 여러 설정의 진입점
-                        // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정을 담당
-                        oauth.userInfoEndpoint(c -> c.userService(oAuth2UserService))
-                                .successHandler(oAuthAuthenticationSuccessHandler)
-                )
-                .addFilterBefore(tokenExceptionFilter, UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new TokenExceptionFilter(), tokenExceptionFilter.getClass()) // 토큰 예외 핸들링
-                .exceptionHandling((exceptions) -> exceptions
-                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
-                        .accessDeniedHandler(new CustomAccessDeniedHandler()));
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(AbstractHttpConfigurer::disable) // cors 비활성화 -> 프론트와 연결 시 따로 설정 필요
+            .httpBasic(AbstractHttpConfigurer::disable) // 기본 인증 로그인 비활성화
+            .formLogin(AbstractHttpConfigurer::disable) // 기본 login form 비활성화
+            .logout(AbstractHttpConfigurer::disable) // 기본 logout 비활성화
+            .headers(c -> c.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable).disable()) // X-Frame-Options 비활성화
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // session 미사용
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/login", "/oauth2/**", " /swagger-ui/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(tokenExceptionFilter, UsernamePasswordAuthenticationFilter.class)
+            .oauth2Login(oauth -> // oauth2 설정 -> OAuth2 로그인 기능에 대한 여러 설정의 진입점
+                             // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때의 설정을 담당
+                             oauth.userInfoEndpoint(c -> c.userService(oAuth2UserService))
+                                 .successHandler(oAuthAuthenticationSuccessHandler)
+            )
+            .addFilterBefore(tokenExceptionFilter, UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(new TokenExceptionFilter(), tokenExceptionFilter.getClass()) // 토큰 예외 핸들링
+            .exceptionHandling((exceptions) -> exceptions
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .accessDeniedHandler(new CustomAccessDeniedHandler()));
 
         return http.build();
     }
