4b9e7b158ce0214b4f5c6c0f2296240da008498b
곽태민 <taminging@kakao.com>
2024-09-10 11:18:48 +0900

    fix: [DEV-004] SecurityConfig.java permitAll /logout 제거

--- src/main/java/com/taemin/user/config/SecurityConfig.java
+++ src/main/java/com/taemin/user/config/SecurityConfig.java
@@ -57,9 +57,7 @@ public class SecurityConfig {
                 .requestMatchers(
                     "/",
                     "/error",
-                    "/oauth2/**",
                     "/auth/login",
-                    "/auth/logout",
                     "/swagger-ui/**",
                     "/v3/api-docs/**",
                     "/favicon.ico").permitAll()
